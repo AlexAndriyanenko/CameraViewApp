e75ef730fb81a7c985a1604f97189ab7285f91b1
Alexey Andriyanenko <a.andriyanenko@do-it.co>
2020-03-20 16:56:43 +0200

    Updates

---
+++ precache-manifest.672e199279ee4ab1ed7b955a7d697e9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9918d63e2676a1dabc21e5252146f01",
+    "url": "/CameraViewApp/index.html"
+  },
+  {
+    "revision": "1405c364d9992afcff8d",
+    "url": "/CameraViewApp/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5e2324ddd39bbdff2f77",
+    "url": "/CameraViewApp/static/css/main.fabea701.chunk.css"
+  },
+  {
+    "revision": "1405c364d9992afcff8d",
+    "url": "/CameraViewApp/static/js/2.c024ddb5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/CameraViewApp/static/js/2.c024ddb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2324ddd39bbdff2f77",
+    "url": "/CameraViewApp/static/js/main.24d8e4a6.chunk.js"
+  },
+  {
+    "revision": "03cfe5c2a5ad8e183a1b",
+    "url": "/CameraViewApp/static/js/runtime-main.1ad40e12.js"
+  },
+  {
+    "revision": "13fff1e5b4ace59adddec44969331604",
+    "url": "/CameraViewApp/static/media/ring.13fff1e5.png"
+  }
+]);
