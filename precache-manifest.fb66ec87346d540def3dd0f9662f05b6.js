46ebbafcf4cceaa37aeb67fa2a1fb6b0e68d29e3
Alexey Andriyanenko <a.andriyanenko@do-it.co>
2020-03-24 11:21:40 +0200

    Updates

--- precache-manifest.fb66ec87346d540def3dd0f9662f05b6.js
+++ precache-manifest.fb66ec87346d540def3dd0f9662f05b6.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "67cab860a70b6332e62c379980470bf7",
+    "revision": "3c2cc1d4993fad3f16ce39101540a2e1",
     "url": "/CameraViewApp/index.html"
   },
   {
-    "revision": "8288b61ebfb3324d14be",
+    "revision": "9cf10bbcac33029bca1b",
     "url": "/CameraViewApp/static/css/2.f39ea103.chunk.css"
   },
   {
-    "revision": "3fb98db6ce5577480373",
+    "revision": "3e8d39bcaba41fbccb82",
     "url": "/CameraViewApp/static/css/main.d0b7a20b.chunk.css"
   },
   {
-    "revision": "8288b61ebfb3324d14be",
-    "url": "/CameraViewApp/static/js/2.b9329ee9.chunk.js"
+    "revision": "9cf10bbcac33029bca1b",
+    "url": "/CameraViewApp/static/js/2.85d15120.chunk.js"
   },
   {
     "revision": "5af347742f42e9ec31fd232ea0d01565",
-    "url": "/CameraViewApp/static/js/2.b9329ee9.chunk.js.LICENSE.txt"
+    "url": "/CameraViewApp/static/js/2.85d15120.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3fb98db6ce5577480373",
-    "url": "/CameraViewApp/static/js/main.9a8739af.chunk.js"
+    "revision": "3e8d39bcaba41fbccb82",
+    "url": "/CameraViewApp/static/js/main.c26dc70e.chunk.js"
   },
   {
     "revision": "03cfe5c2a5ad8e183a1b",
