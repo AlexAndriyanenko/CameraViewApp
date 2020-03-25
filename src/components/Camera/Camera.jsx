e124ea56f5e5de8708edd4cff68a8b0bb6342738
Alexey Andriyanenko <a.andriyanenko@do-it.co>
2020-03-25 16:57:32 +0200

    canvas is now shown, instead on capture video is paused and screenshot is made

--- src/components/Camera/Camera.jsx
+++ src/components/Camera/Camera.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from 'react';
+import React, { createRef } from 'react';
 import Webcam from 'react-webcam';
 import {CAMERA_CONSTRAINTS} from "../../utils/constants";
 import Button from 'react-bootstrap/Button';
@@ -8,29 +8,52 @@ import Col from 'react-bootstrap/Col';
 import ImageOverlay from '../ImageOverlay/ImageOverlay';
 import {faExchangeAlt} from "@fortawesome/free-solid-svg-icons/faExchangeAlt";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import Canvas from "../Canvas/Canvas";
 import './styles.scss';
+import img from "../../assets/ring.png";
 
-const Camera = ({isMobile}) => {
-	const [streamCanvas, setStreamCanvas] = useState(false);
-	const [front, setFront] = useState(false);
-	const webcamRef = useRef(null);
-	const canvasRef = useRef(null);
-	
-	const handleCameraChange = () => {
-		setFront(f => !f);
+class Camera extends React.Component  {
+	constructor(props) {
+		super(props);
+
+		this.webcamRef = createRef();
+		this.canvasRef = createRef();
+		this.imageRef = createRef();
+
+		this.handleCameraChange = this.handleCameraChange.bind(this);
+		this.handleCaptureImage = this.handleCaptureImage.bind(this);
+
+		this.state = {
+			base64: '',
+			front: false,
+			isPaused: false,
+		}
+	}
+
+	componentDidUpdate(prevProps, prevState, snapshot) {
+		if (this.state.isPaused) {
+			this.webcamRef.current.video.pause();
+		} else {
+			this.webcamRef.current.video.play();
+		}
 	};
 
-	const getConstraints = () => {
+
+	handleCameraChange = () => {
+		this.setState((s) => ({
+			front: !s.front,
+		}));
+	};
+
+	getConstraints = () => {
 		let constraints = CAMERA_CONSTRAINTS;
-		if (isMobile) {
+		if (this.props.isMobile) {
 			constraints = {
 				...constraints,
 				video: {
 					...constraints.video,
-					facingMode: front ? 'user' : {exact: 'environment'},
+					facingMode: this.state.front ? 'user' : {exact: 'environment'},
 				},
-				facingMode: front ? 'user' : {exact: 'environment'},
+				facingMode: this.state.front ? 'user' : {exact: 'environment'},
 			};
 		} else {
 			constraints = {
@@ -46,40 +69,93 @@ const Camera = ({isMobile}) => {
 		return constraints;
 	};
 
-	const handleUserMediaStart = () => {
-		setStreamCanvas(true);
+	takeCanvasSnapshot = () => {
+		const ctx = this.canvasRef.getContext('2d');
+
+		const videoWidth = this.webcamRef.current.video.videoWidth;
+		const videoHeight = this.webcamRef.current.video.videoHeight;
+
+		this.canvasRef.width = videoWidth;
+		this.canvasRef.height = videoHeight;
+
+		const imgWidth = 140;
+		const imgHeight = 140;
+
+		const posX = this.canvasRef.width / 2 - imgWidth / 2;
+		const posY = this.canvasRef.height / 2 - imgHeight / 2;
+
+		// first of all clear canvas
+		ctx.clearRect(0, 0, this.canvasRef.width, this.canvasRef.height);
+
+		ctx.drawImage(this.webcamRef.current.video, 0, 0, this.canvasRef.width, this.canvasRef.height);
+		ctx.drawImage(this.imageRef, posX, posY, imgWidth, imgHeight);
 	};
 
-	return (
-		<Container className="camera-container" fluid="md">
-			<Row className="justify-content-md-center mb-5">
-				<Col className="d-flex justify-content-center image-overlay-container">
-					<Webcam
-						ref={webcamRef}
-						className="video-container"
-						audio={false}
-						videoConstraints={getConstraints()}
-						onUserMedia={handleUserMediaStart}
-					/>
-					<ImageOverlay/>
-				</Col>
-			</Row>
-			<Row className="justify-content-md-center mb-3">
-				<Col className="d-flex justify-content-center">
-					<Canvas ref={canvasRef} {...(streamCanvas && ({video: webcamRef.current}))} />
-				</Col>
-			</Row>
-			<Row className="justify-content-md-center">
-				<Col className="d-flex justify-content-center">
-					{isMobile && (
-						<Button onClick={handleCameraChange} varian="light">
-							<FontAwesomeIcon icon={faExchangeAlt}/>
-						</Button>
-					)}
-				</Col>
-			</Row>
-		</Container>
-	)
+	handleCaptureImage = () => {
+		const self = this;
+
+		// if video is currently playing, stop it and make screenshot,
+		// otherwise start playing video
+		if (!this.state.isPaused) {
+			this.takeCanvasSnapshot();
+			this.setState((s) => ({
+				base64: self.canvasRef.toDataURL(),
+			}));
+		}
+
+		this.setState((s) => ({
+			isPaused: !s.isPaused,
+		}))
+	};
+
+	render() {
+		return (
+			<Container className="camera-container" fluid="md">
+				<Row className="justify-content-md-center mb-5">
+					<Col className="d-flex justify-content-center image-overlay-container">
+						<Webcam
+							ref={this.webcamRef}
+							className="video-container"
+							audio={false}
+							videoConstraints={this.getConstraints()}
+						/>
+						<ImageOverlay/>
+					</Col>
+				</Row>
+				<Row className="justify-content-md-center mb-3">
+					<Col className="d-flex justify-content-center">
+						<Container>
+							<Row className="d-flex justify-content-center mb-3">
+								<canvas id="canvas" ref={ref => this.canvasRef = ref}> Your browser doesn't support canvas.</canvas>
+							</Row>
+							<img ref={ref => this.imageRef = ref} src={img} alt="" style={{display: 'none'}}/>
+
+							<Row className="d-flex justify-content-center mb-2">
+								<Button onClick={this.handleCaptureImage}>
+									Capture
+								</Button>
+								{this.state.base64 && (
+									<Button className="btn btn-success ml-2">
+										<a className="text-white" href={this.state.base64}
+										   download> Download </a>
+									</Button>
+								)}
+							</Row>
+						</Container>
+					</Col>
+				</Row>
+				<Row className="justify-content-md-center">
+					<Col className="d-flex justify-content-center">
+						{this.props.isMobile && (
+							<Button onClick={this.handleCameraChange} varian="light">
+								<FontAwesomeIcon icon={faExchangeAlt}/>
+							</Button>
+						)}
+					</Col>
+				</Row>
+			</Container>
+		)
+	}
 };
 
 export default Camera;
